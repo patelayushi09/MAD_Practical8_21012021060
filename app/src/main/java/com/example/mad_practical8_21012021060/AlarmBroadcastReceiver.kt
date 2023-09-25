146ac95217c7383b957bb34a49f89265ef78ad0d
student <student@gnu.ac.in>
2023-09-25 10:47:08 +0530

    pr 8

---
+++ app/src/main/java/com/example/mad_practical8_21012021060/AlarmBroadcastReceiver.kt
@@ -0,0 +1,24 @@
+package com.example.mad_practical8_21012021060
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmBroadcastReceiver : BroadcastReceiver() {
+companion object{
+    val ALARM_KEY="key"
+    val ALARM_STOP="stop"
+    val ALARM_START="start"
+}
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        val data=intent.getStringExtra(ALARM_KEY)
+        val intentService=Intent(context,AlarmService::class.java)
+        if(data== ALARM_START){
+            context.startService(intentService)
+        }
+        else if(data== ALARM_STOP){
+            context.stopService(intentService)
+        }
+    }
+}
--- app/src/main/java/com/example/mad_practical8_21012021060/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical8_21012021060/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.mad_practical8_21012021060
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TimePicker
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val createButton: Button = findViewById(R.id.createAlarmbtn)
+        val cv2: CardView = findViewById(R.id.cardView2)
+        val cancelButton: Button = findViewById(R.id.cancelAlarmbtn)
+        createButton.setOnClickListener {
+            cv2.visibility = View.VISIBLE
+        }
+        cancelButton.setOnClickListener {
+            cv2.visibility = View.GONE
+        }
+
+    }
+
+    fun setAlarm(milliTime: Long, action: String) {
+        val intentAlarm = Intent(applicationContext, AlarmBroadcastReceiver::class.java)
+        intentAlarm.putExtra(AlarmBroadcastReceiver.ALARM_KEY, action)
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            2345,
+            intentAlarm,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val manager = getSystemService(ALARM_SERVICE) as AlarmManager
+        if (action == AlarmBroadcastReceiver.ALARM_START) {
+           manager.setExact(AlarmManager.RTC_WAKEUP,milliTime,pendingIntent)
+        }
     }
 }
